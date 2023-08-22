505254677c61e7c838107abc2b1b992fadb4bafa
Luciano <lucianobenjaminpm@gmail.com>
2023-08-21 20:45:09 -0400

    begin tokio

--- src/components.rs
+++ src/components.rs
@@ -1,24 +1,39 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+
 #[derive(Props, PartialEq )]
 struct PropApp{
         name:&'static str
     }
 pub fn App(cx: Scope) -> Element {
-    cx.render(rsx!{
-        Greet{name: "Luciano"}
-    })
-}
-fn Greet(cx:Scope<PropApp>) -> Element{
     cx.render(rsx!(
-        p{
-            onclick: move |event|{
-               log::info!("Clicked! Event {event:?}");
-               println!("{event:?}")
-                
-            },
-            "Hello, {cx.props.name}"
-        }
-    ))
+			style {include_str!("./style.css")}
+			SendBar(cx)
+		))
+
+}
+fn SendBar(cx: Scope) -> Element{
+	let message = use_state(cx, || "".to_string());
+
+	cx.render(rsx!(
+			form{
+				onsubmit: move 	|e|{
+					if let Some(value) = e.inner().values.get("send").clone(){
+						println!("{:?}",value[0])
+					}
+				},
+				prevent_default:""onsubmit,
+				input{
+					name:"send",
+					class:"input-chat",
+					value: "{message}",
+					oninput: move |e| message.set(e.value.clone())
+				}
+				input{
+					class:"input-submit",
+					r#type:"submit"
+				}
+			}
+	))
 }
